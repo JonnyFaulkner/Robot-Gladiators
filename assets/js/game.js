6cdd8fb6ef46cd6201d42156e8c594ac8c19e9b6
Noctus <jonnyfaulkner21@gmail.com>
2021-05-19 17:13:40 -0500

    added loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,58 +21,54 @@ var enemyAttack = 12;
 
 var fight= function (enemyName) {
     //Alert players that they are starting the round
-    while(enemyHealth > 0) {var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    while(playerHealth > 0 && enemyHealth > 0) {
+        var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+        
+        if (promptFight === "skip" || promptFight === "SKIP") {
+            var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+            
+            if (confirmSkip) {
+                window.alert(playerName + " has decided to skip this fight. Goodbye!");
+                playerMoney = playerMoney - 10;
+                console.log("playerMoney", playerMoney);
+                break;
+            }
+        }
 
-    //if player choses to fight, then fight
-    if (promptFight === "fight" || promptFight === "FIGHT") {
-    enemyHealth = enemyHealth - playerAttack;
-    // Log a resulting message to the console so we know that it worked.
-    console.log(
-        playerName+ " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
-    );
+        //if player choses to fight, then fight
+        enemyHealth = enemyHealth - playerAttack;
+        // Log a resulting message to the console so we know that it worked.
+        console.log(
+            playerName+ " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+        );   
+
+        // check enemy's health
+        if(enemyHealth<= 0) {
+            window.alert(enemyName + " has died!");
+            playerMoney = playerMoney + 20;
+            break;
+        }
+        else {
+            window.alert(enemyName + " still has " + enemyHealth + " health left.");
+        }
     
-    
-    // check enemy's health
-    if(enemyHealth<= 0) {
-        window.alert(enemyName + " has died!");
-    }
-    else {
-        window.alert(enemyName + " still has " + enemyHealth + " health left.");
-    }
-    
 
-    //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
-    playerHealth = playerHealth - enemyAttack;
-    //log a resulting message to the console so we know that it worked
-    console.log(
-        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
-    );
+        //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
+        playerHealth = playerHealth - enemyAttack;
+        //log a resulting message to the console so we know that it worked
+        console.log(
+            enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+        );
 
 
-    // check player's health
-    if (playerHealth <= 0) {
-        window.alert (playerName + " has died!");
-        break;
-    }
-    else {
-        window.alert(playerName + " still has " + playerHealth + " health left.");
-    }
-
-    //if player choses to skip
-}   else if (promptFight === "skip" || promptFight === "SKIP") {
-    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-    
-    if (confirmSkip) {
-        window.alert(playerName + " has decided to skip this fight. Goodbye!");
-        playerMoney = playerMoney - 2;
-    }
-    else {
-        fight();
-    }
-
-}   else {
-    window.alert("You need to choose a valid option. Try again!")
-}
+        // check player's health
+        if (playerHealth <= 0) {
+            window.alert (playerName + " has died!");
+            break;
+        }
+        else {
+            window.alert(playerName + " still has " + playerHealth + " health left.");
+        }
     }
 };
 
