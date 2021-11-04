258dd7adfb9fa50be4a0e583f7ddc111f87e5eab
Jonathan Faulkner <83264372+JonnyFaulkner@users.noreply.github.com>
2021-11-04 17:22:35 -0500

    added local storage saving for high score

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,15 @@
-var fight= function (enemy) {
-    
+var fight = function (enemy) {
+
     //Alert players that they are starting the round
-    while(playerInfo.health > 0 && enemy.health > 0) {
+    while (playerInfo.health > 0 && enemy.health > 0) {
         var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
-        
+
         if (promptFight === "skip" || promptFight === "SKIP") {
             var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-            
+
             if (confirmSkip) {
                 window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
-                playerInfo.money = Math.max (0, playerInfo.money - 10);
+                playerInfo.money = Math.max(0, playerInfo.money - 10);
                 console.log("playerMoney", playerInfo.money);
                 break;
             }
@@ -22,10 +22,10 @@ var fight= function (enemy) {
 
         console.log(
             playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
-        );   
+        );
 
         // check enemy's health
-        if(enemy.health<= 0) {
+        if (enemy.health <= 0) {
             window.alert(enemy.name + " has died!");
             playerInfo.money = playerInfo.money + 20;
             break;
@@ -33,13 +33,13 @@ var fight= function (enemy) {
         else {
             window.alert(enemy.name + " still has " + enemy.health + " health left.");
         }
-    
+
 
         //Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable.
         var damage = randomNumber(enemy.attack - 3, enemy.attack);
 
         playerInfo.health = Math.max(0, playerInfo.health - damage);
-        
+
         console.log(
             enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
         );
@@ -51,15 +51,15 @@ var fight= function (enemy) {
             endGame();
             break;
         }
-    }        
+    }
 };
 
-var startGame = function() {
+var startGame = function () {
     playerInfo.reset();
 
-    for(var i = 0; i < enemyInfo.length; i++) {
+    for (var i = 0; i < enemyInfo.length; i++) {
         if (playerInfo.health > 0) {
-            window.alert("Welcome to Robot Gladiators! Round " + ( i + 1 ));
+            window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
             var pickedEnemyObj = enemyInfo[i];
             pickedEnemyObj.health = randomNumber(40, 60);
             fight(pickedEnemyObj);
@@ -77,9 +77,23 @@ var startGame = function() {
     endGame();
 };
 
-var endGame = function() {
+var endGame = function () {
     if (playerInfo.health > 0) {
-        window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+        window.alert("The game has now ended. Let's see how you did!");
+
+        var highScore = localStorage.getItem("highscore");
+        if (highScore === null) {
+            highScore = 0;
+        }
+
+        if (playerInfo.money > highScore) {
+            localStorage.setItem("highscore", playerInfo.money);
+            localStorage.setItem("name", playerInfo.name);
+
+            alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+        } else {
+            alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!")
+        }
     }
     else {
         window.alert("You've lost your robot in battle.");
@@ -95,7 +109,7 @@ var endGame = function() {
     };
 }
 
-var shop = function() {
+var shop = function () {
     var shopOptionPrompt = window.prompt(
         "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE, or 'LEAVE' to make a choice."
     );
@@ -113,13 +127,13 @@ var shop = function() {
             window.alert("Leaving the store.");
             break;
         default:
-            window.alert ("You did not pick a valid option. Try again.");
+            window.alert("You did not pick a valid option. Try again.");
             shop();
             break;
     }
 };
 
-var randomNumber = function(min, max) {
+var randomNumber = function (min, max) {
     var value = Math.floor(Math.random() * (max - min + 1) + min);
     return value;
 };
@@ -129,16 +143,15 @@ var playerInfo = {
     health: 100,
     attack: 10,
     money: 10,
-    reset: function() {
-        if(this.money >= 7) {
-            window.alert("Refilling player's health by 20 for 7 dollars.");
+    reset: function () {
+        if (this.money >= 7) {
             this.health = 100;
             this.money = 10;
             this.attack = 10;
         }
     },
-    refillHealth: function() {
-        if(this.money >= 7) {
+    refillHealth: function () {
+        if (this.money >= 7) {
             window.alert("Refilling player's health by 20 for 7 dollars.");
             this.health += 20;
             this.money -= 7;
@@ -147,7 +160,7 @@ var playerInfo = {
             window.alert("You don't have enough money!");
         }
     },
-    upgtadeAttack: function() {
+    upgradeAttack: function () {
         if (this.money >= 7) {
             window.alert("Upgrading player's attack by 6 for 7 dollars.");
             this.attack += 6;
@@ -163,16 +176,16 @@ var playerInfo = {
 var enemyInfo = [
     {
         name: "Roborto",
-        attack: randomNumber(10,14)
+        attack: randomNumber(10, 14)
     },
     {
         name: "Amy Android",
-        attack: randomNumber(10,14)
+        attack: randomNumber(10, 14)
     },
     {
         name: "Robo Trumble",
-        attack: randomNumber(10,14)
+        attack: randomNumber(10, 14)
     }
 ];
 
-startGame ()
+startGame()
